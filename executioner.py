e7585ae1ce0d43dda078bebfc2bb0ea89636f544
davidsaccavino <david.saccavino@gmail.com>
2023-02-25 18:29:57 -0500

    Changelog: - Added executioner.py to run the algotrader in a scheduled loop - Added packages.txt to list the required packages for the algotrader

---
+++ executioner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import schedule, time, subprocess
+
+def job(t):
+    print("Parsing Data")
+    subprocess.run(["python", "broker.py"])
+    return
+
+schedule.every().day.at("09:45").do(job,'It is 09:45')
+
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
