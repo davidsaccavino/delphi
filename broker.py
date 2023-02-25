2cd15c4a2dc32a3dbc7a9f6919dd44cc1ca253fc
davidsaccavino <david.saccavino@gmail.com>
2023-02-25 16:00:11 -0500

    Changelog: - Created a json folder and moved the json files into it - Created the logic gates for parsing the indicators in broker.py - Created the logic gates for handling the market orders in broker.py - Added SuperTrend to the list of indicators - Added an 8-day SMA of to the list of indicators - Added a Stochastic to the list of indicators

--- broker.py
+++ broker.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-
+import delphi
 from alpaca.trading.enums import OrderSide, TimeInForce
 from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.client import TradingClient
@@ -9,15 +9,84 @@ load_dotenv()
 
 trading_client = TradingClient(os.getenv('PAPER-API-KEY'), os.getenv('PAPER-SECRET-KEY'))
 
-# preparing order data
-market_order_data = MarketOrderRequest(
-                      symbol="AAPL",
-                      qty=1,
-                      side=OrderSide.BUY,
-                      time_in_force=TimeInForce.DAY
-                  )
 
-# Market order
-market_order = trading_client.submit_order(
-                order_data=market_order_data
-                )
+
+obv = delphi.getOBV()
+smma = delphi.getOBVSMMA()
+stochK, stochD = delphi.getStochastic()
+macdLine, signalLine = delphi.getMACD()
+supertrend = delphi.getSuperTrend()
+eightSMA = delphi.get8BarSMA()
+
+
+# Start of: Logic gates for determining bullish or bearish sentiment
+
+# OBV Bullish/Bearish logic gates
+bullishOBV = obv[-1] > smma[-1]
+bearishOBV = obv[-1] < smma[-1]
+
+# MACD Bullish/Bearish logic gates
+bullishMACD = macdLine.iloc[-1] > signalLine.iloc[-1] + 0.25
+bearishMACD = macdLine.iloc[-1] < signalLine.iloc[-1] - .05
+
+# Stochastic Bullish/Bearish logic gates
+bullishStochastic = stochK.iloc[-1] > 60 and stochK.iloc[-1] > (stochD.iloc[-1] + 1.5)
+bearishStochastic = (stochK.iloc[-1] < 60 and stochD.iloc[-1] > (stochK.iloc[-1] + 1.5)) or stochK.iloc[-1] < 40
+
+# Aggregated Oscillator Bullish/Bearish logic gates
+bullishOscillators = bullishOBV and bullishStochastic and bullishMACD
+bearishOscillators = bearishOBV and bearishStochastic and bearishMACD
+
+# Volume-Oscillator-weighted SuperTrend buy and sell signals
+VOSTbuySignal = (supertrend.iloc[-1]["SUPERTd_11_3.0"] == 1 and bullishOscillators and delphi.storedDataframe['close'].iloc[-1] > eightSMA.iloc[-1])
+VOSTsellSignal = (supertrend.iloc[-1]["SUPERTd_11_3.0"] == -1 and supertrend.iloc[-2]["SUPERTd_11_3.0"] == 1 and bearishOscillators and delphi.storedDataframe['close'].iloc[-1]  < eightSMA.iloc[-1])
+
+# print(supertrend[:-1])
+# Determine if currently in a "De-Risk Zone"
+deRisk = (not VOSTsellSignal) and (stochK.iloc[-1] < 55 or delphi.storedDataframe['close'].iloc[-1] < eightSMA)
+
+# End of: Logic gates for determining bullish or bearish sentiment
+
+
+# preparing order data
+if VOSTbuySignal and not deRisk:
+    market_order_data = MarketOrderRequest(
+        symbol="QQQ",
+        qty=1,
+        side=OrderSide.BUY,
+        time_in_force=TimeInForce.DAY
+    )
+
+        # Market order
+    market_order = trading_client.submit_order(
+        order_data=market_order_data
+        )
+
+
+
+if VOSTsellSignal:
+    market_order_data = MarketOrderRequest(
+        symbol="QQQ",
+        qty=1,
+        side=OrderSide.SELL,
+        time_in_force=TimeInForce.DAY
+    )
+
+    # Market order
+    market_order = trading_client.submit_order(
+        order_data=market_order_data
+        )
+
+
+if deRisk:
+    market_order_data = MarketOrderRequest(
+        symbol="QQQ",
+        qty=1,
+        side=OrderSide.SELL,
+        time_in_force=TimeInForce.DAY
+    )
+
+    # Market order
+    market_order = trading_client.submit_order(
+        order_data=market_order_data
+        )
--- delphi.py
+++ delphi.py
@@ -3,57 +3,64 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 import pandas as pd
+import pandas_ta as ta
 import numpy as np
+
+# Prevent usage of exponential notation
 pd.options.display.float_format = '{:,.4f}'.format
 
-
-
+# Load environment variables
 load_dotenv()
 
 
-storedValues = {}
+# Fetch historical data from local json file
 storedDataframe = pd.DataFrame()
-with open('QQQ_Historical_Values.json', 'r+') as f:
-    storedValues = json.load(f)
-    storedDataframe = pd.DataFrame(storedValues)
-
-
-
-
+with open('./json/QQQ_Historical_Values.json', 'r+') as f:
+    storedDataframe = pd.DataFrame(json.load(f))
 
 
 
 # Calculate Moving Average Convergence Divergence
 def getMACD():
-    # closing_prices = getClosingPrices()
 
     # Calculate fast and slow exponential moving averages (EMAs)
     fast_ema = storedDataframe[["close"]].ewm(span=12, adjust=False).mean(numeric_only=True)
     slow_ema = storedDataframe[["close"]].ewm(span=26, adjust=False).mean(numeric_only=True)
     macd = fast_ema - slow_ema
 
+
     # Caclulate the signal line and MACD spread
     signal_line = macd.ewm(span=9, adjust=False).mean()
-    macdSpread = macd - signal_line
 
 
-    return macdSpread
+    return macd, signal_line
+
+
+
 
 # Calculate On-Balance-Volume
 def getOBV():
+
     i = 0
     obv = []
 
     for i in range(len(storedDataframe['close'])):
+
         if i == 0:
             value = storedDataframe['OnBalanceVolume'][i]
             obv.append(value)
+
+
         elif storedDataframe['close'][i] > storedDataframe['close'][i - 1] :
             value = obv[-1] + storedDataframe['Volume'][i]
             obv.append(value)
+
+
         elif storedDataframe['close'][i] < storedDataframe['close'][i - 1]:
             value = obv[-1] - storedDataframe['Volume'][i]
             obv.append(value)
+
+
         elif storedDataframe['close'][i] == storedDataframe['close'][i - 1]:
             value = obv[-1]
             obv.append(value)
@@ -61,16 +68,59 @@ def getOBV():
     return obv
 
 
+
+
 # Calculate Smoothed Moving Average of OBV
 def getOBVSMMA():
+
     obv_len = 8
 
+    # 600678000 is the initial value of the OBV as it is the first value in the json file
     initial_ma = 600678000
     initial_smma = (initial_ma * (obv_len - 1) + storedDataframe['OnBalanceVolume'][obv_len]) / obv_len
     smma = [initial_smma]
 
+
     for i in range(1, len(storedDataframe['OnBalanceVolume'])):
         smma.append((smma[i - 1] * (obv_len - 1) + storedDataframe['OnBalanceVolume'][i]) / obv_len)
 
+
     return smma
+
+
+
+
+def getStochastic():
+
+    # Calculate the highest high and lowest low over the past 14 days
+    highest_high = storedDataframe['high'].rolling(window=14).max()
+    lowest_low = storedDataframe['low'].rolling(window=14).min()
+
+
+    # Calculate %K, unsmoothed
+    stochK = (storedDataframe['close'] - lowest_low) / (highest_high - lowest_low) * 100
+    # Smooth %K
+    stochK = stochK.rolling(window=3).mean()
+
+
+    # Calculate %D
+    stochD = stochK.rolling(window=3).mean()
+
+
+    return round(stochK, 2), round(stochD, 2)
+
+
+
+
+def getSuperTrend():
+    supertrend = ta.supertrend(high = storedDataframe['high'], low = storedDataframe['low'], close = storedDataframe['close'], length = 11, multiplier = 3, offset = -10)
+    return supertrend.iloc[:-11, :]
+
+
+
+
+def get8BarSMA():
+    sma = storedDataframe['close'].rolling(window=8).mean(numeric_only=True).dropna()
+
+    return sma
 
